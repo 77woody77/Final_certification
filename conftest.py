138660f389cfedce3ea664014114a4a141c2f38c
oleg Zhollobov <jolobov777@mail.ru>
2024-06-18 19:37:00 +0500

    Add Firefox, Edge

--- conftest.py
+++ conftest.py
@@ -3,7 +3,10 @@ import requests
 import yaml
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from webdriver_manager import chrome, firefox, microsoft
 from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
 
 with open('./testdata.yaml') as f:
     testdata = yaml.safe_load(f)
@@ -11,11 +14,24 @@ with open('./testdata.yaml') as f:
 
 @pytest.fixture(scope="function")
 def browser():
-    service = Service(executable_path=ChromeDriverManager().install())
-    options = webdriver.ChromeOptions()
-    driver = webdriver.Chrome(service=service, options=options)
-    yield driver
-    driver.quit()
+    if testdata["browser"] == "chrome":
+        service = Service(executable_path=ChromeDriverManager().install())
+        options = webdriver.ChromeOptions()
+        driver = webdriver.Chrome(service=service, options=options)
+        yield driver
+        driver.quit()
+    elif testdata["browser"] == "firefox":
+        service = Service(executable_path=GeckoDriverManager().install())
+        options = webdriver.FirefoxOptions()
+        driver = webdriver.Firefox(service=service, options=options)
+        yield driver
+        driver.quit()
+    elif testdata["browser"] == "microsoft":
+        service = Service(executable_path=EdgeChromiumDriverManager().install())
+        options = webdriver.EdgeOptions()
+        driver = webdriver.Edge(service=service, options=options)
+        yield driver
+        driver.quit()
 
 @pytest.fixture()
 def login():
